260b4ee00762ccad67736d6fc4247815fdb72de3
NoemiRA <201821077@tese.edu.mx>
2021-03-23 16:52:49 -0600

    Listado de Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero}from "./hero";
+
+
+
+export const HEROES: Hero[] = [
+{id: 11, name: "Superman", edad: 30, poder: "Volar y fuerza", universo:"DC", identidad:"Clark Kent"},
+{id: 12, name: "Batman", edad: 30, poder:'Ingenio', universo:"Marvel", identidad:"Bruce Wayne"},
+{id: 13, name: "Hulk", edad: 40, poder:"Fuerza fisica", universo:"Marvel", identidad:"Bruce Banner"},
+{id: 14, name: "Ironman", edad: 49, poder:"Super Traje", universo:"Marvel", identidad:"Tony Stark"},
+{id: 15, name: "Thor", edad: 1000, poder:"Fuerza sobrehumana", universo: "Marvel", identidad:"Donald Blake"},
+{id: 16, name: "Spiderman", edad: 20, poder:"Aracnido", universo:"Marvel", identidad:"Peter Parker"},
+{id: 17, name: "Black Widow", edad: 28, poder:"Combate Marcial", universo:"Marvel", identidad:"Natalia alianovna Romanova"},
+{id: 18, name: "Flash", edad: 28, poder:"Velocidad", universo:"DC", identidad:"Barry Allen"},
+{id: 19, name: "Bruja Escarlata", edad: 36, poder:"Proyeccion Astral", universo:"Marvel", identidad:"Wanda Maximoff"},
+{id: 20, name: "Wonder Woman", edad: 30, poder:"Lazo Magico", universo:"DC",identidad:"Diana prince"}
+];
